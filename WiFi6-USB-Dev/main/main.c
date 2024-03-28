4ad8a14ac375741f226418340360f9c7b3511de4
KevinXZJ <1764931534@qq.com>
2024-03-28 23:45:55 +0800

    Add WiFi-USB NAT project

---
+++ WiFi6-USB-Dev/main/main.c
@@ -0,0 +1,53 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-08-22
+ *
+ * @copyright Copyright (c) 2023
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include "board.h"
+
+#include "log.h"
+#include "shell.h"
+#include <lwip/tcpip.h>
+
+#include "bflb_uart.h"
+
+#include "wifi_event.h"
+#include "wifi6_led.h"
+#include "user_state.h"
+#include "user_esflash.h"
+
+#ifdef DBG_TAG
+#undef DBG_TAG
+#define DBG_TAG "MAIN"
+#endif
+
+extern void shell_init_with_task(struct bflb_device_s *shell);
+
+static struct bflb_device_s *uart0;
+
+int main(void)
+{
+    board_init();
+
+    uart0 = bflb_device_get_by_name("uart0");
+    shell_init_with_task(uart0);
+
+    esay_flash_init();
+    led_init();
+    user_state_start();
+    tcpip_init(NULL, NULL);
+    wifi_start_firmware_task();
+    user_state_send_notify(STATE_SYSTEM_START, 0);
+    vTaskStartScheduler();
+}
